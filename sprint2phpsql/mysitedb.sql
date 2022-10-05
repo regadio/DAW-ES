443147cf3b0a55c1fa362599deac417a114d74df
Nombre Apellido <tu.email@fpcoruna.afundacion.org>
2022-10-05 14:38:57 +0200

    Base de datos creada

--- sprint2phpsql/mysitedb.sql
+++ sprint2phpsql/mysitedb.sql
@@ -16,6 +16,27 @@ create table tPeliculas{
 create table tComentarios{
     id int primary key auto_increment,
     comentario varchar(50),
-    usuario_id int foreign key,
-    pelicula_id int not null foreign key
+    usuario_id int,
+    pelicula_id int not null
+    FOREIGN key(usuario_id) REFERENCES tUsuarios (id),
+    FOREIGN key(pelicula_id) REFERENCES tPeliculas (id)
+   
 }
+
+insert into tUsuarios values("0","Nombre1", "Apellido1", "email1", "1234");
+insert into tUsuarios values("0","Nombre2", "Apellido2", "email2", "1234");
+insert into tUsuarios values("0","Nombre3", "Apellido3", "email3", "1234");
+insert into tUsuarios values("0","Nombre4", "Apellido4", "email4", "1234");
+insert into tUsuarios values("0","Nombre5", "Apellido5", "email5", "1234");
+
+insert into tPeliculas values("0", "Interestellar", "https://pics.filmaffinity.com/interstellar-366875261-large.jpg");
+insert into tPeliculas values("0", "Minions", "https://pics.filmaffinity.com/minions_the_rise_of_gru-608394212-large.jpg");
+insert into tPeliculas values("0", "Origen", "https://pics.filmaffinity.com/inception-652954101-mmed.jpg");
+insert into tPeliculas values("0", "Avatar", "https://pics.filmaffinity.com/avatar-832187025-mmed.jpg");
+insert into tPeliculas values("0", "Tadeo Jones 3", "https://pics.filmaffinity.com/tadeo_jones_3_la_tabla_esmeralda-118107760-msmall.jpg");
+
+insert into tUsuarios values("0","comentario1", "Apellido1", "email1", "1234");
+insert into tUsuarios values("0","comentario2", "Apellido2", "email2", "1234");
+insert into tUsuarios values("0","comentario3", "Apellido3", "email3", "1234");
+insert into tUsuarios values("0","comentario4", "Apellido4", "email4", "1234");
+insert into tUsuarios values("0","comentario5", "Apellido5", "email5", "1234");
