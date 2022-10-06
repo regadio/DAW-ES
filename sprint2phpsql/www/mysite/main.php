d698a3e20305f962018f2678dc2ce74fa8876906
Nombre Apellido <tu.email@fpcoruna.afundacion.org>
2022-10-06 12:01:04 +0200

    Prueba

--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -1,9 +1,14 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="main.css">
+	<title>Document</title>
 </head>
 <body>
-	<h1>Prueba</h1>
-	<?php
+ <?php
 		$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 	?>
 
@@ -15,9 +20,32 @@
 			echo '<br>';
 			echo $row[2];
 			echo '<br>';
+			echo'<div class="tarjeta">'
+					'<div class="titulo">'
+						 .$row['nombre'];
+					'</div>'
+					'<div class="cuerpo">'
+						<img src="" alt="muestra">
+					'</div>'
+					'<div class="pie">'
+						id
+					'</div>'
+				'</div>'
+			;
 		}
-	?>
-
+	?> 
+	<div class="tarjeta">
+		<div class="titulo">
+			echo $row['nombre'];
+		</div>
+		<div class="cuerpo">
+		<img src="" alt="muestra">
+		
+		</div>
+		<div class="pie">
+		id
+		</div>
+	</div>
 
 </body>
 </html>
