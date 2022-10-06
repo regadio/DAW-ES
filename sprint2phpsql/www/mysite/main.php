19a4876c206a84f740549c45813c6f5cdf8b0d1b
Nombre Apellido <tu.email@fpcoruna.afundacion.org>
2022-10-06 11:35:48 +0200

    Prueba 4.4

--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -6,15 +6,18 @@
 	<?php
 		$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 	?>
+
 	<?php
 		$query = 'SELECT * FROM tPeliculas';
-		mysqli_query($db, $query) or die('Query error');
-	?>
-	<?php
-		$query = 'SELECT * FROM tCanciones';
 		$result = mysqli_query($db, $query) or die('Query error');
 		while ($row = mysqli_fetch_array($result)) {
+			echo $row['nombre'];
+			echo '<br>';
+			echo $row[2];
+			echo '<br>';
 		}
 	?>
+
+
 </body>
 </html>
