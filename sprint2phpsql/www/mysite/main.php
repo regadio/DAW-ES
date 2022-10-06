bb22571df284228b9ba59cb9ae1ec7917be60a5c
Nombre Apellido <tu.email@fpcoruna.afundacion.org>
2022-10-06 11:00:24 +0200

    mod

--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -2,9 +2,10 @@
 <head>
 </head>
 <body>
-	<h1>Prueba</h1>
+	
 	<?php
 		$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+		<h1>Prueba</h1>
 	?>
 	<?php
 		$query = 'SELECT * FROM tPeliculas';
