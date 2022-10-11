60cc15a6e25633a6db7f3fb27dd870a88409708b
Nombre Apellido <tu.email@fpcoruna.afundacion.org>
2022-10-11 13:56:33 +0200

    Prueba

--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -1,29 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="main.css">
-	<title>PHPSQL</title>
-</head>
-<body>
- 	<?php
-		$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
-	?>
-
-	<?php
-		$query = 'SELECT * FROM tPeliculas';
-		$result = mysqli_query($db, $query) or die('Query error');
-		while ($row = mysqli_fetch_array($result)) {?>
-			<!-- <div class="tarjeta">
-				<?php $row['nombre']?>
-			</div> -->
-			<tr>
-				<td><?php $row['nombre']?></td>
-			</tr>
-			<?php $row['nombre']?>
-	<?php } ?>
-
-</body>
-</html>
+<link rel="stylesheet" href="/main.css" />
+<?php
+$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+?>
+<h1>Conexión establecida</h1>
+<?php
+// Lanzar una query
+$query = 'SELECT * FROM tJuegos';
+$result = mysqli_query($db, $query) or die('Query error');
+echo '<table style="text-align: center;">';
+echo '<td colspan="3" style="background-color: #7677E8;">Good Games</td>';
+// Recorrer el resultado
+while ($row = mysqli_fetch_array($result)) {
+  echo '<tr>';  
+          echo '<td style="background-color: #7677E8" width="80" >';
+                echo $row['id'];
+        echo '</td>';
+        echo '<td>';
+                echo $row['nombre'];
+        echo '</td>';
+        echo '<td>';
+                echo '<img src=" '.$row[2].'"  width="80" height="120">';
+        echo '</td>';
+  echo '</tr>';
+}
+echo '</table>';
+mysqli_close($db);
+?>
