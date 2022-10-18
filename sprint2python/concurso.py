115751c14751211e6b022d63e3951a8cf977af62
Nombre Apellido <tu.email@fpcoruna.afundacion.org>
2022-10-18 13:19:40 +0200

    Ejercicio 1 Python Terminado

---
+++ sprint2python/concurso.py
@@ -0,0 +1,9 @@
+print("¿Cuántos años tiene Fernando Alonso?")
+print("a) 47")
+print("b) 30")
+print("c) 41")
+respuesta=input("Tu respuesta es (a/b/c): ")
+if respuesta=="47" or respuesta.lower()=="a":
+    print("¡Has acertado!")
+else:
+    print("Has fallado")
