62ed5a8a5c99fdc503d0031931fac3134ef29333
Nombre Apellido <tu.email@fpcoruna.afundacion.org>
2022-10-18 13:40:47 +0200

    Ejercicio 2 Python Terminado Bien

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -34,12 +34,3 @@ else:
     puntos = puntos - 5
 print("Tu puntuacion es: ",puntos)
 
-# print("¿Cuántos Oscar ha ganado 'El retorno del Rey'?")
-# print("a) 12")
-# print("b) 11")
-# print("c) 13")
-# respuesta=input("Tu respuesta es: ")
-# if respuesta=="11":
-#     print("¡Has acertado!")
-# else:
-#     print("Has fallado")
