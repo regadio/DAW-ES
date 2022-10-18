8be2ea75799ec1c90dbbf54ad5353bc81370c78c
Nombre Apellido <tu.email@fpcoruna.afundacion.org>
2022-10-18 13:50:35 +0200

    Ejercicio 5 Python Terminado

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,7 +1,26 @@
+from factorial import func_factorial
+
 from factorial2 import func_factorial2
 
-num = int(input("Introduzca número positivo: "))
-if num < 0:
-    print("Número negativo, no válido.")
+print("Calculo de Factoriales")
+
+print("a) Usando Recursividad.")
+
+print("b) Sin Usar Recursividad.")
+
+op=input("Elija una opción (a/b): ")
+
+if op.lower() == "a":
+    num = int(input("Introduzca un número positivo: "))
+    if num < 0:
+        print("Número negativo, no válido.")
+    else:
+        print("El factorial de ",num," es: ",func_factorial(num))
+elif op.lower() == "b":
+    num = int(input("Introduzca un número positivo: "))
+    if num < 0:
+        print("Número negativo, no válido.")
+    else:
+        print("El factorial de ",num," es: ",func_factorial2(num))
 else:
-    print("El factorial de ",num," es: ",func_factorial2(num))
+    print("Esta opción no existe")
