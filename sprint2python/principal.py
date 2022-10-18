62cec77910c36636b3ae5e8fc7657be5ff9797b9
Nombre Apellido <tu.email@fpcoruna.afundacion.org>
2022-10-18 13:58:11 +0200

    Ejercicio 6 Python Terminado

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,6 +1,6 @@
 from factorial import func_factorial
-
 from factorial2 import func_factorial2
+import time
 
 print("Calculo de Factoriales")
 
@@ -15,12 +15,20 @@ if op.lower() == "a":
     if num < 0:
         print("Número negativo, no válido.")
     else:
+        start_time = time.time()
         print("El factorial de ",num," es: ",func_factorial(num))
+        end_time = time.time()
+        elapsed_time = end_time - start_time    
+        print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
 elif op.lower() == "b":
     num = int(input("Introduzca un número positivo: "))
     if num < 0:
         print("Número negativo, no válido.")
     else:
+        start_time = time.time()
         print("El factorial de ",num," es: ",func_factorial2(num))
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
 else:
     print("Esta opción no existe")
