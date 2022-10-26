6c7ac4cc98adae6e8edb6a2f792c5948fe441616
Nombre Apellido <tu.email@fpcoruna.afundacion.org>
2022-10-26 13:45:37 +0200

    Ejercicio 1 #S3 Termiando

---
+++ sprint3sesiones/www/mysite/comment.php
@@ -0,0 +1,24 @@
+<?php 
+	$db = mysqli_connect('localhost','root','1234','mysitedb') or die ('Fail');
+?>
+<html>
+<body>
+	<?php 
+		$id_pelicula = $_POST['pelicula_id'];
+		$comentario = $_POST['new_comment'];
+
+		$query = "INSERT INTO tcomentario(comentario,id_pelicula, id_usuario, fecha) VALUES ('"
+			.$comentario."',".$id_pelicula.",NULL, now())";
+		mysqli_query($db,$query) or die('Error');
+
+		echo "<p>Nuevo comentario ";
+		echo mysqli_insert_id($db);
+		echo " añadido</p>";
+
+		echo "<a href='/detail.php?id=" 
+		.$id_pelicula." '>Volver</a>";
+		mysqli_close($db);
+	?>
+
+</body>
+</html>
---
+++ sprint3sesiones/www/mysite/main.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./main.css">
+	<title>Document</title>
+</head>
+<body>
+<?php
+      $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+    ?>
+    <?php
+      // Lanzar una query
+      $query = 'SELECT * FROM tpelicula';
+      $result = mysqli_query($db, $query) or die('Query error');
+      echo '<table style="text-align: center;">';
+        echo '<td colspan="3" style="background-color: #F5C858;">Lista de Peliculas</td>';
+        // Recorrer el resultado
+        while ($row = mysqli_fetch_array($result)) {
+          echo '<tr>';  
+                echo '<td style="background-color: #F5C858" width="80" >';
+                        echo $row['id_pelicula'];
+                echo '</td>';
+                echo '<td>';
+                  echo'<a href="/detail.php?id='.$row['id_pelicula'].'">'.$row['nombre'].'</a>';
+                echo '</td>';
+                echo '<td>';
+                        echo '<img src=" '.$row[2].'"  width="80" height="120">';
+                echo '</td>';
+          echo '</tr>';
+        }
+    echo '</table>';
+    mysqli_close($db);
+    ?>
+</body>
+</html>
