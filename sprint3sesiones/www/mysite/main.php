7c5d6a86300a21f472b4d29a16e3b30ffb4d6649
regadior <regadior@gmail.com>
2022-10-27 18:13:30 +0200

    Ejercicio 7 #S3 Termiando

--- sprint3sesiones/www/mysite/main.php
+++ sprint3sesiones/www/mysite/main.php
@@ -7,6 +7,26 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="./main.css">
 	<title>Document</title>
+  <style>
+    table, tr,td,th{
+      background-color: #dbdbdb;
+      border: 1px solid black;
+      border-collapse: collapse;
+    }
+    img:hover {
+					transition: 0.5s all ease-in-out;
+					transform: scale(1.1);
+				}
+        a{
+          text-decoration: none;
+          color: black;
+        }
+        #uno:hover{
+          background-color:#dbdbdb;
+				  font-size:17px;
+          font-weight: bold;
+        }
+  </style>
 </head>
 <body>
 <a href="/logout.php">Logout</a>
@@ -19,13 +39,18 @@
       $result = mysqli_query($db, $query) or die('Query error');
       echo '<table style="text-align: center;">';
         echo '<td colspan="3" style="background-color: #F5C858;">Lista de Peliculas</td>';
+        echo '<tr>';  
+          echo '<th>Id</th>';
+          echo '<th>Nombre</th>';
+          echo '<th>Imagen</th>';
+        echo '</tr>';
         // Recorrer el resultado
         while ($row = mysqli_fetch_array($result)) {
           echo '<tr>';  
                 echo '<td style="background-color: #F5C858" width="80" >';
                         echo $row['id_pelicula'];
                 echo '</td>';
-                echo '<td>';
+                echo '<td id="uno">';
                   echo'<a href="/detail.php?id='.$row['id_pelicula'].'">'.$row['nombre'].'</a>';
                 echo '</td>';
                 echo '<td>';
