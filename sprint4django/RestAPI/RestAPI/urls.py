78c543ccda0c761c06dc5e708178f6aa5e9c7bcf
Nombre Apellido <tu.email@fpcoruna.afundacion.org>
2022-11-08 14:01:56 +0100

    Ejercicio 1 Django 2º get Terminado

--- sprint4django/RestAPI/RestAPI/urls.py
+++ sprint4django/RestAPI/RestAPI/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('test', views.pagina_de_prueba),
     path('peliculas', views.devolver_peliculas),
+    path('peliculas/<int:id_solicitado>', views.devolver_pelicula_por_id),
+    
 ]
--- sprint4django/RestAPI/webserviceapp/views.py
+++ sprint4django/RestAPI/webserviceapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 from .models import Tpelicula
-
+from .models import Tcomentario
 # Create your views here.
 def pagina_de_prueba(request):
 	return HttpResponse("<h1>Hola caracola</h1>");
@@ -18,3 +18,21 @@ def devolver_peliculas(request):
 		diccionario['publicaación'] = fila_sql.publicaación
 		respuesta_final.append(diccionario)
 	return JsonResponse(respuesta_final, safe=False) 
+
+def devolver_pelicula_por_id(request, id_solicitado):
+	pelicula = Tpelicula.objects.get(id_pelicula = id_solicitado)
+	comentarios = pelicula.tcomentario_set.all()
+	lista_comentarios = []
+	for fila_comentario_sql in comentarios:
+		diccionario = {}
+		diccionario['id_comentario'] = fila_comentario_sql.id_comentario
+		diccionario['comentario'] = fila_comentario_sql.comentario
+		lista_comentarios.append(diccionario)
+	resultado = {
+		'id_pelicula': pelicula.id_pelicula,
+		'nombre': pelicula.nombre,
+		'url_imagen': pelicula.url_imagen,
+		'publicaación': pelicula.publicaación,
+		'comentario': lista_comentarios
+	}
+	return JsonResponse(resultado, json_dumps_params={'ensure_ascii': False})
