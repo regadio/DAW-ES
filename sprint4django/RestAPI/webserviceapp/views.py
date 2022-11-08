9a969e6ac35e287281ecb8da960be0293e38de30
Nombre Apellido <tu.email@fpcoruna.afundacion.org>
2022-11-08 12:40:00 +0100

    Solucion error

--- sprint4django/RestAPI/webserviceapp/views.py
+++ sprint4django/RestAPI/webserviceapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from djanfo.http import HttpResponse
+from django.http import HttpResponse
 
 def pagina_de_prueba(request):
 	return HttpResponse("<h1>Hola caracola</h1>");
